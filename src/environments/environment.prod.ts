8922e64f1c86f44ffaccb10e6082fd51ac29aa08
Jean Marc Bassahak <bjm.graphicdesigner@gmail.com>
2025-03-06 01:41:55 +0100

    Modify env to local wordpress

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  url: 'https://wemos.net/wp-json/wc/v3',
-  consumerKey: 'ck_8b7ead34d31e3ea575260477951758c547f3ce67',
-  consumerSecret: 'cs_64449cfe1ccce2935fddc42d7e0c2169bf124bab',
-  urlBlog: 'https://wemos.net/wp-json/wp/v2',
+  url: 'https://test-api.test/wp-json/wc/v3',
+  consumerKey: 'ck_64175cd76e14817c01e38757e899a0f8ed8eba5e',
+  consumerSecret: 'cs_0b06a794e0f06096810314e1e2bf5a817fd9570d',
+  urlBlog: 'https://test-api.test/wp-json/wp/v2',
 };
